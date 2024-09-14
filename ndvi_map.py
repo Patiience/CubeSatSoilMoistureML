27c56088cd80554ba162f0add38bc36a6abfa4e0
Daniel Lu <88689263+Patiience@users.noreply.github.com>
2024-09-14 14:03:00 -0400

    Update ndvi_map.py

--- ndvi_map.py
+++ ndvi_map.py
@@ -3,57 +3,69 @@
 from netCDF4 import Dataset
 import numpy as np
 import os
-import matplotlib.pyplot as plt
-from mpl_toolkits.basemap import Basemap
-#import cartopy.crs as ccrs
+import pickle
 
-# Initialize file path to .nc file
-directory_path = "/data01/jyin/NOAA20VI"
+# Function reads data and then dumps into a binary file
+def read_map_data(directory_path):
+  # Loop through 2019, 2021, 2022, 2023, 2024 directories
+  for year in os.listdir(directory_path):
+    # Path to year directory
+    year_path = os.path.join(directory_path, year)
+    # Loop through each date directory
+    for date in os.listdir(year_path):
+      # Path to date directory
+      date_path = os.path.join(year_path, date)
+      # Loop through files in date directory
+      for files in os.listdir(date_path):
+        # Path to file
+        file_path = os.path.join(date_path, files)
 
-# Loop through 2019, 2021, 2022, 2023, 2024 directories
-# for year in os.listdir(directory_path):
-#   year_path = os.path.join(directory_path, year)
-#   for date in os.listdir(year_path):
-#     date_path = os.path.join(year_path, date)
-#     for file in os.listdir(date_path):
-#       file_path = os.path.join(date_path, file)
-#       nc_file = Dataset(file_path)
-file_path = "/data01/jyin/NOAA20VI/2019/20190715/VI-DLY-REG_v1r2_j01_s20190713_e20190713_c201907150108040.nc"
+        #file_path = "/data01/jyin/NOAA20VI/2019/20190715/VI-DLY-REG_v1r2_j01_s20190713_e20190713_c201907150108040.nc"
 
-# Open the .nc file(s)
-nc_file = Dataset(file_path)
+        # Create Dataset Object & open the .nc file(s)
+        nc_file = Dataset(file_path)
 
-# Retrieve NDVI_TOC data, in 2 dimensional (lat, lon)
-nc_data = nc_file.variables['NDVI_TOC'][:,:]
-lat = nc_file.variables['latitude'][:,:]
-lon = nc_file.variables['longitude'][:,:]
-# Unmask the data
-nc_data = np.array(nc_data)
+        # Initialize NDVI grid, for 1 km resolution
+        ndvi_grid = np.zeros((18000, 36000))
 
-# Plot the data
-plt.figure(figsize=(10, 5))
+        # Retrieve NDVI_TOC data, in 2 dimensional (lat, lon), as well as lat and lon coordinates
+        nc_data = nc_file.variables['NDVI_TOC'][:,:]
+        lat = nc_file.variables['latitude'][:,:]
+        lon = nc_file.variables['longitude'][:,:]
 
-# Create a Basemap instance
-m = Basemap(projection='cyl', resolution='c', 
-            llcrnrlat=-90, urcrnrlat=90, 
-            llcrnrlon=-180, urcrnrlon=180)
+        # Unmask the data
+        nc_data = np.array(nc_data)
 
-# Draw coastlines and other map features
-m.drawcoastlines()
-m.drawcountries()
-m.drawparallels(np.arange(-90, 91, 30), labels=[1,0,0,0])
-m.drawmeridians(np.arange(-180, 181, 60), labels=[0,0,0,1])
+        # Turn FILL_VALUE into -9999
+        nc_data[nc_data == -32768] = -9999
 
-# Convert the latitude and longitude grid to map projection coordinates
-lon, lat = np.meshgrid(lon, lat)
-x, y = m(lon, lat)
+        # Loop through nc_data and assign to grid accordingly
+        for i in range(len(nc_data)):
+          for j in range(len(nc_data[0])):
+            # If already zero, don't need to update grid
+            if nc_data[i][j] != 0:
+              # Convert lat and lon to x,y grid points
+              x = lat[i][j]/.01
+              y = lon[i][j]/.01
 
-# Plot the data
-m.pcolormesh(x, y, nc_data, shading='auto', cmap='viridis')  # Replace 'data' with your actual data
+              # Assign to grid
+              ndvi_grid[y][x] = nc_data[i][j]
 
-plt.title('NDVI for 7/15/2019')
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
+        # For each file, dump into binary file in data01 directory
+        binary_path = '/data01/dlu12/NDVI_Binaries'
+        binary_file = f'NOAA20_TOC_NDVI_{files}.dat'
+        binary_path = os.path.join(data_path, binary_file)
 
-plt.savefig('/home/dlu12/NDVI_test.png')
-plt.show()
+        with open(binary_path, 'wb') as f:
+          pickle.dump(ndvi_grid, f)
+
+        # Close the file
+        nc_file.close()
+
+# Main function
+if __name__ == "__main__":
+  # Initialize file path to .nc file
+  directory_path = "/data01/jyin/NOAA20VI"
+
+  # Read the data
+  read_map_data(directory_path)
