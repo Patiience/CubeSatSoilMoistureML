02466a7ec727c82abde6fc283264b4c7c73fc03c
Daniel Lu <88689263+Patiience@users.noreply.github.com>
2024-11-04 15:31:09 -0500

    Update ml_inputs.py

--- ml_inputs.py
+++ ml_inputs.py
@@ -20,6 +20,18 @@ def read_data(directory_path, files):
       data = np.fromfile(file_path, dtype=np.float32) # Default little-endian
       data = data.reshape((18000,36000)) # Landcover covers whole global domain
       data = np.flipud(data)  # Flip LandCover data since it's upside down
+
+      # Intialize lat and lon range to match domain for others
+      # -60 to 90 N for lat & -180 to 180 E lon
+      # Addition is to adjust lat and lon values to adjust in correspondance to MODIS dataset indices
+      # -90 to 90 N for lat & -180 to 180 E lon
+      lat1 = int((-60 + 90) / 0.01)
+      lat2 = int((90 + 90)/ 0.01)
+      lon1 = int((-180 + 180) / 0.01)
+      lon2 = int((180 + 180) / 0.01)
+
+      # Retrieve the correct domain
+      data = data[lat1: lat2, lon1: lon2]
     
     # Mask -9999
     data = np.ma.masked_equal(data, -9999)
@@ -40,32 +52,19 @@ def read_data(directory_path, files):
     plt.figure(figsize=(10, 5))
 
     # Create a Basemap instance
-    if data_type != "LandCover":
-      m = Basemap(projection='cyl', resolution='c', 
-                  llcrnrlat=-60, urcrnrlat=90, 
-                  llcrnrlon=-180, urcrnrlon=180)
-    else:
-      m = Basemap(projection='cyl', resolution='c', 
-                  llcrnrlat=-90, urcrnrlat=90, 
-                  llcrnrlon=-180, urcrnrlon=180)
+    m = Basemap(projection='cyl', resolution='c', 
+                llcrnrlat=-60, urcrnrlat=90, 
+                llcrnrlon=-180, urcrnrlon=180)
 
     # Draw coastlines and other map features
     m.drawcoastlines()
     m.drawcountries()
-
-    if data_type != "LandCover":
-      m.drawparallels(np.arange(-60, 91, 10), labels=[1,0,0,0])
-    else:
-      m.drawparallels(np.arange(-90, 91, 10), labels=[1,0,0,0])
-
+    m.drawparallels(np.arange(-60, 91, 10), labels=[1,0,0,0])
     m.drawmeridians(np.arange(-180, 181, 20), labels=[0,0,0,1])
 
     # Create latitude and longitude arrays that match grid
     lon = np.linspace(-180, 180, data.shape[1])
-    if data_type != "LandCover":
-      lat = np.linspace(-60, 90, data.shape[0])
-    else:
-      lat = np.linspace(-90, 90, data.shape[0])
+    lat = np.linspace(-60, 90, data.shape[0])
 
     # Convert the latitude and longitude grid to map projection coordinates
     lon, lat = np.meshgrid(lon, lat)
