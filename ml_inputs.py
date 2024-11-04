236d4a2365472f7e5dfade9c8a86287672051397
Daniel Lu <88689263+Patiience@users.noreply.github.com>
2024-11-04 15:39:49 -0500

    Update ml_inputs.py

--- ml_inputs.py
+++ ml_inputs.py
@@ -84,6 +84,82 @@ def read_data(directory_path, files):
     plt.savefig(f'/home/dlu12/{data_type}.png')
     plt.close()
 
+# Define function to process MODIS data
+def process_modis(directory_path):
+  # Sort directory to ensure we get correct order of files
+    directory = os.listdir(directory_path)
+
+    # Only include years from 2018-2024
+    included_years = {'2018', '2019', '2020', '2021', '2022', '2023', '2024'}
+
+    # Filter the directory list to include only specified years
+    filtered_directory = [item for item in directory if item in included_years]
+
+    # Sort the remaining items
+    sorted_directory = sorted(filtered_directory)
+
+    # List used for testing later
+    file_list = []
+
+    # Loop through each year's directory
+    for year in sorted_directory:
+        # File path
+        year_path = os.path.join(directory_path, year)
+
+        # Sort the files in the directory for matching purposes with SMNDVI
+        files = os.listdir(year_path)
+        sorted_files = sorted(files)
+
+        # For each year exclude, 12/31, since SMNDVI data does not include it
+        sorted_files = sorted_files[:-1]
+
+        # If the year is 2018, exclude all days before 2018 5th Week or 20180129
+        if year == "2018":
+            sorted_files = sorted_files[28:] # Note: 28th index since first index is 0
+        elif year == "2024":
+            # If the year is 2024, exclude all days after 20240331, SMNDVI only goes up to there, MODIS goes to 20240422
+            sorted_files = sorted_files[:-22] 
+    
+        # Loop through the files in the year's directory
+        for files in sorted_files:
+            # File path
+            file_path = os.path.join(year_path, files)
+
+              # Print statement for tracking progress
+            print(f"Loading data for {files}")
+
+            # Load in data from binary file, pickle & joblib throw errors, so use numpy
+            # Will load it in as a memory map to avoid memory errors
+            ndvi_grid = np.fromfile(file_path, dtype='<f4')
+            ndvi_grid = ndvi_grid.reshape((15000,36000))
+
+            # Intialize lat and lon range for the NWM domain
+            # NWM domain is from 20, 60 lat & -140, -60 lon
+            # Addition is to adjust lat and lon values to match MODIS dataset indices
+            # -60 to 90 N for lat & -180 to 180 E lon
+            lat1 = int((20 + 60) / 0.01)
+            lat2 = int((60 + 60)/ 0.01)
+            lon1 = int((-140 + 180) / 0.01)
+            lon2 = int((-60 + 180) / 0.01)
+
+            # Retrieve the NWM domain through indexing
+            ndvi_grid = ndvi_grid[lat1: lat2, lon1: lon2]
+
+            # Mask -9999
+            ndvi_grid = np.ma.masked_equal(ndvi_grid, -9999)
+
+            #################################################################################
+            # NEED TO FIGURE OUT WHAT TO DO HERE, HOW DO WE STORE EACH OF THE DATA POINTS ###
+             '''
+            sum_modis_y = sum_modis_y + ndvi_grid
+            sum_modis_y2 = sum_modis_y2 + (ndvi_grid ** 2)
+            N = N + 1
+
+            # Append file path to files list for evaluation later
+            file_list.append(file_path)
+            '''
+
+
 # Main function
 if __name__ == "__main__":
   # Initialize file path to .nc file
@@ -97,3 +173,7 @@ if __name__ == "__main__":
 
   # Read the data
   read_data(directory_path, files)
+
+  # Process Modis
+  modis_directory = "/data01/jyin/MODISNDVI/Gapfilling1kmNDVI"
+  process_modis(modis_directory)
